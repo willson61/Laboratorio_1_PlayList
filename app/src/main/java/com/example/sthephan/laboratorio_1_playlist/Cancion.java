348ab575ae6cf41730397c8caf7b7913fe2f39e7
willson61 <31348545+willson61@users.noreply.github.com>
2018-08-30 08:34:45 -0600

    Iniciando laboratorio y agregando clase Cancion

---
+++ app/src/main/java/com/example/sthephan/laboratorio_1_playlist/Cancion.java
@@ -0,0 +1,41 @@
+package com.example.sthephan.laboratorio_1_playlist;
+
+
+public class Cancion {
+    public String nombre;
+    public String autor;
+    public int anno;
+    public double tiempo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+}
---
+++ app/src/main/java/com/example/sthephan/laboratorio_1_playlist/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.sthephan.laboratorio_1_playlist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtNombre)
+    EditText txtNombre;
+    @BindView(R.id.swOrden)
+    Switch swOrden;
+    @BindView(R.id.lstCanciones)
+    ListView lstCanciones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnBuscar, R.id.btnAgregar})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnBuscar:
+                break;
+            case R.id.btnAgregar:
+                break;
+        }
+    }
+}
